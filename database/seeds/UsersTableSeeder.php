ad6d45f72c2141c9352df45a014f720f378f1b49
dharmendrapargaien <dharmendrapargaien@gmail.com>
2016-07-19 18:04:59 +0530

    chenge credential for user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,8 +15,8 @@ class UsersTableSeeder extends Seeder
 
             \App\Models\User::firstOrCreate([
                 'name'       => 'dharmendra',
-                'email'      => 'david.dharmendra@ithands.net',
-                'password'   => '$2y$10$4ns4kUFFaKRDGXm4nwXxB.pwjXqOnD6i5B4B79bKGhVO8vPSOb0Ke',//@dmin@123
+                'email'      => 'dharmendrapargaien@gmail.com',
+                'password'   => bcrypt('@dmin@123'),
                 'role_id'    => 1 
             ]);
         }
